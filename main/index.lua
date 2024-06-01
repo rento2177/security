06ec7200bd1e88fa4a5364503da0f3275e96185f
Rento <109392019+rento2177@users.noreply.github.com>
2024-06-01 20:51:05 +0900

    Update index.lua

--- main/index.lua
+++ main/index.lua
@@ -1,1 +1,157 @@
-print("Hello World!");
+local domain = gg.makeRequest("https://scrty.netlify.app/cfg/projectName");
+local source = gg.makeRequest("https://scrty.netlify.app/main/index.lua");
+local func = gg.makeRequest("https://scrty.netlify.app/main/func.lua");
+local _, type = xpcall(loadfile(path.."inputType.lua"), function(fw)
+    fw = io.open(path.."inputType.lua", "w");
+    fw:write("return \"number\";");
+    fw:close();
+    return "number";
+end);
+--[[
+if not ggsx then
+    gg.alert("[x4.1] ggsxの読み込みに失敗しました。");
+    os.exit();
+elseif domain.code ~= 200 then
+    ggsx.logCatch("[main.lua] スクリプトの単体実行", false);
+    os.exit();
+elseif source.code == 200 and (function(fr)
+    fr = io.open(gg.getFile():match("[^/]+$"), "r");
+    local cash = fr:read("a");
+    fr:close();
+    return cash;
+end)() ~= source.content then
+    ggsx.logCatch("[main.lua] プログラム改竄", true);
+    os.remove(gg.getFile());
+    os.exit();
+end
+dm = dm:gsub("\n", "");
+ggsx.logGuard(co.content);
+ggsx.net = true;
+]]
+
+function Main()
+    local mn1 = gg.choice({
+        "基礎メニュー", 
+        "需要しかないメニュー", 
+        "APIを取得する", 
+        "スクリプト設定", 
+        "終了する"}, 2024, "れんれん");
+    if mn1 then _ENV["mn"..mn1+1]();end
+end
+
+function mn2()
+    local mn2 = gg.prompt({
+        "猫缶 [ON]", 
+        "猫缶"..typeb("[0;58999]"), 
+        "XP [ON]", 
+        "XP"..typeb("[0;999999999]"), 
+        "通常チケット [ON]", 
+        "レアチケット [ON]", 
+        "通常チケット"..typeb("[0;999]"), 
+        "レアチケット"..typeb("[0;999]"), 
+        "即勝利", 
+        "ステージ開放", 
+        "メインに戻る"
+    }, {
+        [2] = 58000, 
+        [4] = 777777777, 
+        [7] = 200, 
+        [8] = 200
+    }, typec(11, {2, 4, 7, 8}));
+
+    
+end
+
+function mn3()
+    local mn3 = gg.prompt({
+        "全キャラ開放", 
+        "全キャラレベル [ON]", 
+        "レベル変更"..typeb("[0;999]"), 
+        "全キャラ形態 [ON]", 
+        "形態変更"..typeb("[1;4]"), 
+        "指定キャラまとめ", 
+        "エラーキャラ削除", 
+        "お宝解放", 
+        "NP [ON]", 
+        "NP"..typeb("[0;99999]"), 
+        "アイテム [ON]", 
+        "アイテム"..typeb("[0;9999]"), 
+        "キャッツアイ [ON]", 
+        "キャッツアイ"..typeb("[0;999]"), 
+        "ネコビタン [ON]", 
+        "ネコビタン"..typeb("[0;999]"), 
+        "城の素材 [ON]", 
+        "城の素材"..typeb("[0;999]"), 
+        "メインに戻る"
+    }, {
+        [3] = 20, 
+        [5] = 2, 
+        [10] = 2000, 
+        [12] = 2000, 
+        [14] = 200, 
+        [16] = 200, 
+        [18] = 200
+    }, typec(19, {3, 5, 10, 12, 14, 16, 18}));
+
+
+end
+
+function mn4()
+
+end
+
+function mn5()
+    local mn5 = gg.choice({
+        "利用規約", 
+        "入力形式の変更", 
+        "スクリプト再起動", 
+        "メインに戻る"}, 2024, "れんれん");
+
+
+end
+
+function mn6()
+    --事前の数値と範囲を復元
+    print("Script制作者: 蓮斗");
+    gg.setVisible(true);
+    os.exit();
+end
+
+function typeb(rge)
+    return type == "number" and "" or rge;
+end
+
+function typec(len, tbl)
+    local n, cash = 1, {};
+    for i = 1, len do
+        cash[i] = tbl[n] == i and "number" or "checkbox";
+        n = cash[i] == "number" and n + 1 or n;
+    end
+    return cash;
+end
+
+function Execute(_FUNC, _ARGU, cash)
+    if not pcall(_ENV[_FUNC](_ARGU)) then
+        
+    end
+end
+
+::start::
+local page = true;
+local rest = {["rang"] = gg.getRanges(), ["val"] = gg.getResults(20)};
+if not pcall(function()ggsx.logGuard(func.content)();end) then
+    gg.alert("関数の読み込みに失敗しました。");
+    gg.setVisible(true);
+    os.exit();
+end
+test();
+--ベース値設定
+
+
+while true do
+    if gg.isVisible() or page then
+        gg.setVisible(false);
+        page = false;
+        Main();
+    end
+end
