c4af34997675eb8bdc47a5ed7aaeefb5cbf70679
Rento <109392019+rento2177@users.noreply.github.com>
2024-06-02 14:58:01 +0900

    Update func.lua

--- main/func.lua
+++ main/func.lua
@@ -3,10 +3,12 @@ function test()
   return "成功";
 end
 
-function p22()
+function p22(v)
+  print(v)
   gg.toast("成功");
 end
 
-function p31()
+function p31(v)
+  print("これは使われない", v);
   gg.toast("成功");
 end
