d75768be826cd4b921cc0a4458d794d53c4b8d9c
Rento <109392019+rento2177@users.noreply.github.com>
2024-06-04 20:02:15 +0900

    Update index.lua

--- main/index.lua
+++ main/index.lua
@@ -1,4 +1,4 @@
+print(gg.getFile():match("[^/]+$"));
 local fr = io.open(gg.getFile():match("[^/]+$"), "r");
-    local cash = fr:read("a");
-    fr:close();
-print("通過", cash);
+print(fr:read("a"));
+fr:close();
