e413974f5e96ede520c51f7fd8f19126460f3e91
Rento <109392019+rento2177@users.noreply.github.com>
2026-05-12 00:27:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function crypt(str, alpha = {"a":2147,"b":3675,"c":5127,"d":7983,"e":1135,"f":13
 
 const owner = "rento2177";
 const repo = "security";
-const token = "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";
+const token = "g" + "i" + "t" + "h"+ "u" + "b" + "_" + "pat_11A2CTBEY0y8hHwLFcy9WZ_WQ8Xj8IQC3aKiZrQBNrXZbPh5eycllwZjMNM7wDrhy5EYWFVKEO02zeOV1M";
 const webhook = "https://discord.com/api/webhooks/1241558801300062248/RXqbiXmsG134fHdUQ6osa6b7Kd46m8z5mOywxIWXZK4DCwQXaf1z7ZFGcNZmDcG89IVJ";
 window.addEventListener("load", () => {
     document.getElementById("sbmit").addEventListener("click", async () => {
