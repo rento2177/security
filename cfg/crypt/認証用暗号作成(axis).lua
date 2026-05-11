ecfba9b0671f6e4748f003b90ac3951eadec4524
Rento <109392019+rento2177@users.noreply.github.com>
2026-05-11 23:49:57 +0900

    Delete cfg directory

--- cfg/crypt/認証用暗号作成(axis).lua
+++
@@ -1,9 +0,0 @@
---対象ファイルの実行後このスクリプトを実行する
-
-api = gg.makeRequest("https://scrty.glitch.me", nil, '{"type": "ango"}');
-
-print(api.content);
-
-local fw = io.open("./axis", "w");
-fw:write(api.content);
-fw:close();
